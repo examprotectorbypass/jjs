7b885078ecaa60208cc508c2e5ff90102b67408a
examprotectorbypass <examprotectorbypass@gmail.com>
2026-03-17 04:05:29 +0500

    1.bat

--- 1.bat
+++ 1.bat
@@ -28,6 +28,149 @@ error1.exe -n "SafeExamBrowser.Client.exe" -i "C:\Program Files (x86)\Microsoft\
 cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
 error1.exe -n "SafeExamBrowser.Server.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\one.dll"
 
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "etschrome.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\LicenseChecker.dll"
+
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "lockdownbrowseroem.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\LicenseChecker.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "lockdownbrowser.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\LicenseChecker.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "javaw.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\LicenseChecker.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "proproctor.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\LicenseChecker.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "questionmark secure for windows desktop.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\LicenseChecker.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "SafeExamBrowser.Client.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\LicenseChecker.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "SafeExamBrowser.Server.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\LicenseChecker.dll"
+
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "etschrome.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "lockdownbrowseroem.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "lockdownbrowser.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "javaw.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "proproctor.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "questionmark secure for windows desktop.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "SafeExamBrowser.Client.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "SafeExamBrowser.Server.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "SafeExamBrowser.Server.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "psi-bastion.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "psi-keygrab.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\AffinityKillerDLL.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "etschrome.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "lockdownbrowseroem.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "lockdownbrowser.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "javaw.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "proproctor.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "questionmark secure for windows desktop.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "SafeExamBrowser.Client.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "SafeExamBrowser.Server.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "SafeExamBrowser.Server.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "psi-bastion.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error3.exe -n "psi-keygrab.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\64.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "etschrome.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "lockdownbrowseroem.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "lockdownbrowser.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "javaw.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "proproctor.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "questionmark secure for windows desktop.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "SafeExamBrowser.Client.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "SafeExamBrowser.Server.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "etschrome.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "lockdownbrowseroem.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "lockdownbrowser.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "javaw.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "proproctor.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "questionmark secure for windows desktop.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "SafeExamBrowser.Client.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
+
+cd C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games
+error1.exe -n "SafeExamBrowser.Server.exe" -i "C:\Program Files (x86)\Microsoft\Edge\Application\WindowGames\games\32.dll"
 
 @echo off
 echo %time%
