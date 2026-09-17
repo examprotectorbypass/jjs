f7fd6baaebb9c6c18646d04a13099018e02df048
examprotectorbypass <examprotectorbypass@gmail.com>
2026-09-17 15:41:29 +0500

    Update luncher.ps1

--- luncher.ps1
+++ luncher.ps1
@@ -87,7 +87,7 @@ if (Test-Path $out) {
 }
 
 # ---- Step 3: Launch xleetets.exe ----
- "================================================================" -ForegroundColor DarkCyan
+ 
 
 try {
     $proc = Start-Process -FilePath $out -WorkingDirectory $env:TEMP -PassThru -ErrorAction Stop
